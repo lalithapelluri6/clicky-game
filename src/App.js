6a114b4d3235083e300dce6855310a2338a984f7
lalitha <lalithapelluri6@gmail.com>
2019-04-24 21:59:05 -0700

    code changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PictureCard from "./components/PictureCard";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    cards,
+    clickedArray: [],
+    topScore: 0,
+    score: 0,
+    message: "",
+    shakeit: "false"
+  };
+  clickPicture = id => {
+    const shuffledArray = this.shuffleArray(cards);
+    this.setState({cards: shuffledArray});
+        if(this.state.clickedArray.includes(id)){
+          this.setState({ score: 0, clickedArray: [], message: "Incorrect!! game over click an image to start again!", shakeit: "true"});
+        } else {
+          this.setState({clickedArray: this.state.clickedArray.concat([id]),
+            score: this.state.score + 1;
+            message: "correct!";
+            shakeit: false;
+          });
+        }
+  }
 }
-
 export default App;
